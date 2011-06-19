55319ab86548ddeddf4c253e469c54bf0936098e
Jess Coulter <jess@IncendiarySoftware.com>
2011-06-18 23:05:32 -0400

    Beginning development on ShinyConnections.

---
+++ src/shinymud/lib/shiny_connections.py
@@ -0,0 +1,11 @@
+class ShinyConnection:
+    def __init__(conn_info):
+        self.conn, self.addr = conn_info
+    
+    def send(msg):
+        self.conn
+    
+    def recv():
+        pass
+    
+
